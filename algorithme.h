d8870ee089c48a2332c1e2c13b1eea5b41cc92de
Elysio-creator <samy.68100@gmail.com>
2024-11-21 13:29:27 +0100

    Update algorithme.h

---
+++ algorithme.h
@@ -0,0 +1,26 @@
+#ifndef ALGOMAINDROITE_H
+#define ALGOMAINDROITE_H
+#include "Robot.h"
+
+
+
+class Algorythme{
+    private:
+        Robot d_robot;
+    public:
+        Algorythme(Robot r); 
+
+        void algoMainDroite();
+        void algoPledge();   
+    
+};
+
+
+
+
+
+
+
+
+
+#endif
