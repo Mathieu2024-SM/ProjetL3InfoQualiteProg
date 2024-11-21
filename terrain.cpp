c70e85da503e32af6242cb2c66045ad0d3f9dc5f
Mathieu2024-SM <mathieu.schellenbaum@gmail.com>
2024-11-21 14:45:59 +0100

    Add files via upload

---
+++ terrain.cpp
@@ -0,0 +1,69 @@
+#include "terrain.h"
+#include <fstream>
+
+Terrain::Terrain(int hauteur , int largeur) : d_grille{}, d_hauteur{hauteur}, d_largeur{largeur}
+{}
+Terrain::Terrain() : d_grille{}, d_hauteur{0}, d_largeur{0}
+{}
+
+int Terrain::hauteur()const{
+    return d_hauteur;
+}
+int Terrain::largeur()const{
+    return d_largeur;
+}
+
+void Terrain::chargerTerrainDepuis(std::string chemin){
+
+    std::ifstream fichierterrain(chemin);
+    
+    if (!fichierterrain){
+        std::cerr<<"erreur de lecture"<<std::endl;
+        return;
+    }
+
+    d_grille.clear();
+    std::string ligne;
+    while(std::getline(fichierterrain,ligne)){
+        d_grille.push_back(std::vector<char>(ligne.begin(),ligne.end()));
+    }
+    d_hauteur=d_grille.size();
+    d_largeur=d_grille[0].size();
+}
+
+void Terrain::sauverTerrainSur(std::string chemin)const{
+    std::ofstream fichierterrain(chemin);
+    
+    if (!fichierterrain){
+        std::cerr<<"erreur de lecture"<<std::endl;
+        return;
+    }
+    for(auto& ligne:d_grille){
+        for (char colonne:ligne){
+            fichierterrain<<colonne;  
+        }
+        fichierterrain<<"\n";
+        
+    }        
+
+}
+
+void Terrain::afficherTerrain()const{
+    for(auto& ligne:d_grille){
+        for (auto& colonne:ligne){
+            std::cout<<colonne<<' ';
+        }
+        std::cout<<std::endl;
+    }
+}
+
+
+void Terrain::donnerElementGrille(int x , int y, char ch){
+    d_grille[x][y]=ch;
+}
+
+char Terrain::afficherElementGrille(int x, int y){
+    return d_grille[x][y];
+}
+
+
---
+++ terrain.h
@@ -0,0 +1,35 @@
+#ifndef TERRAIN_H
+#define TERRAIN_H
+
+#include <vector>
+#include <iostream>
+#include <string>
+
+/**
+	Elements dans un terrain
+		'X' = un mur --> le robot ne doit pas aller dessus
+		'.' = une case vide --> le robot peut aller dessus
+		'A' = case d'arriver --> case où le robot doit arriver
+*/
+
+class Terrain{
+    private:
+        std::vector<std::vector<char>> d_grille; // Un tableau de caractère
+        int d_hauteur,d_largeur; // Hauteur et largeur du terrain
+
+    public:
+        Terrain(int hauteur , int largeur); // Constructeur
+        Terrain(); // Constructeur par defaut
+		
+		int hauteur()const;
+        int largeur()const; 
+		
+        void chargerTerrainDepuis(std::string chemin);
+        void sauverTerrainSur(std::string cheminFichier) const;
+        void afficherTerrain() const;
+		
+        void modifierElementGrille(int x , int y, char ch);
+        char afficherElementGrille(int x, int y);   
+};
+
+#endif
