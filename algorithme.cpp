c478547849e970b0dadf6aed1a91f5e9feb6a6a0
Elysio-creator <samy.68100@gmail.com>
2024-12-15 16:58:46 +0100

    Add files via upload

--- algorithme.cpp
+++ algorithme.cpp
@@ -1,25 +1,53 @@
 #include "algorithme.h"
-
+#include "terrain.h"
 
 
 Algorithme::Algorithme(Robot r){
     this->d_robot=r;
 }
-void AlgorithmeMainDroite::typeAlgo(){
+void AlgoMainDroite::typeAlgo(Terrain terrain){
 
-    while(d_robot.d_terrain.getGrille(d_robot.getX(),d_robot.getY())!='S'){
-        if(d_robot.isObstacleDevant()){
-            d_robot.turnLeft();
+    while(terrain.afficherElementGrille(d_robot.getX(),d_robot.getY())!='A'){
+        if(d_robot.ObstacleDevant(terrain)){
+            d_robot.tourneAGauche();
 
         }
-        if(!d_robot.isObstacleDevant()){
-            while(!d_robot.isObstacleDevant()){
-                d_robot.forward();
+        if(!d_robot.ObstacleDevant(terrain)){
+            while(!d_robot.ObstacleDevant(terrain)){
+                d_robot.Avance();
             }
         }
-        if(!d_robot.isObstacleDroite()){
-            d_robot.turnRight();
+        if(!d_robot.ObstacleDroite(terrain)){
+            d_robot.tourneADroite();
         }
-        d_robot.forward();
+        d_robot.Avance();
+    }
+}
+
+
+void AlgoPledge::typeAlgo(Terrain terrain){
+    int compteur =0;
+    while(terrain.afficherElementGrille(d_robot.getX(),d_robot.getY())!='A'){
+        
+        while(!d_robot.ObstacleDevant(terrain)){
+            d_robot.Avance();
+        }
+        do{
+            
+            if(d_robot.ObstacleDevant(terrain)){
+                d_robot.tourneAGauche();
+                compteur++;
+            }
+            else if(!d_robot.ObstacleDroite(terrain)){
+                d_robot.tourneADroite();
+                compteur--;
+                if(!d_robot.ObstacleDevant(terrain)){
+                    d_robot.Avance();
+                }         
+            }
+            else{
+                d_robot.Avance();
+            }
+        }while(compteur!=0);      
     }
 }
