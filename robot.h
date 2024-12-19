882b8a6cc05ba15ff930ce1a95509dac41e4109e
lE6CANT68 <98109831+lE6CANT68@users.noreply.github.com>
2024-12-19 12:35:56 +0100

    Update robot.h

--- robot.h
+++ robot.h
@@ -9,23 +9,22 @@
 class Robot {
 private:
     int x, y;
-    char d_forme;
     int d_ort;
 
 public:
-    Robot(int xInitial, int yInitial ,char forme);
+    Robot(int xInitial, int yInitial);
     Robot();
     int getX() const;
     int getY() const;
-    void afficherposition(int newX, int newY);
+    void afficherposition (int newX, int newY)const;
 
     int orientation()const;
     void tourneAGauche();
     void tourneADroite();
     void Avance();
     
-    bool ObstacleDevant(Terrain t);
-    bool ObstacleDroite(Terrain t);
+    bool ObstacleDevant(Terrain t)const;
+    bool ObstacleDroite(Terrain t)const;
 
      
 
