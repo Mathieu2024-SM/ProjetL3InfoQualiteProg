2c9ffd92808efbc61f96c8d546bc6b24924e2913
lE6CANT68 <98109831+lE6CANT68@users.noreply.github.com>
2024-12-19 13:36:27 +0100

    Update robotmover.cpp
    ajout recuperatio etat

--- robotmover.cpp
+++ robotmover.cpp
@@ -3,10 +3,12 @@
 void robotGauche::deplaceRobot(Robot& r)
 {
     r.tourneAGauche();
+     // r.recupereEtat();
 }
 void robotDroite::deplaceRobot(Robot& r)
 {
     r.tourneADroite();
+   // r.recupereEtat();
 }
 robotAvance::robotAvance(int pas):d_pas{pas}
 {
@@ -15,4 +17,5 @@ robotAvance::robotAvance(int pas):d_pas{pas}
 void robotAvance::deplaceRobot(Robot& r)
 {
     r.Avance();
+     // r.recupereEtat();
 }
